48ad90d8b778c60c1e8bf5d56cd7cc4f1a475fd3
Your Name <justinejade.sulo@cvsu.edu.ph>
2025-02-19 18:43:08 +0800

    Frontend changes last

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
   app.enableCors({ 
-    origin: "https://kimballllly.github.io/Student-management/", // Change this to match your frontend's port
+    origin: ['https://kimballllly.github.io', 'https://kimballllly.github.io/Student-management/'],
     methods: "GET,POST,PUT,DELETE",
     allowedHeaders: "Content-Type, Authorization"
   });
 
   await app.listen(3000);
-  app.enableCors();
-
 }
+
 bootstrap();
