e3bbe50dc3c8aecea0626d5d5f2f8d6d83e2516a
Your Name <justinejade.sulo@cvsu.edu.ph>
2025-02-19 18:23:05 +0800

    Changed port

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({ 
-    origin: "http://localhost:3001", // Change this to match your frontend's port
+    origin: "http://localhost:3000", // Change this to match your frontend's port
     methods: "GET,POST,PUT,DELETE",
     allowedHeaders: "Content-Type, Authorization"
   });
 
   await app.listen(3000);
+  app.enableCors();
+
 }
 bootstrap();
